015aebdc9be44e7285d09c1b52e034e69ffe380b
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-15 17:14:29 +0800

    Refactor activation of drop-down

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ class DropDown {
         const selector = document.querySelector(`#${name} > .dropDownSelector`);
         const dropDown = document.querySelector(`#${name} .dropDown`);
         const options = Array.from(document.querySelector(`#${name} > .dropDown`).children);
-
-        this.whenClicked(selector, () => {dropDown.hidden = false});
+        
+        this.whenClicked(selector, () => {console.log("meow");dropDown.classList.add("active")});
         this.whenClicked(window, (e) => {
             if (options.includes(e.target)) console.log(e.target.textContent);
-            if (e.target !== dropDown && e.target !== selector) dropDown.hidden = true;
+            if (e.target !== dropDown && e.target !== selector) dropDown.classList.remove("active");
         });
     }
 
