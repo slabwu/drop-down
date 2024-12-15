0adb64ab837db04f84a60db7dcc8d8ddf90eac55
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-15 16:28:30 +0800

    Refactor event listeners

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ class DropDown {
         const dropDown = document.querySelector(`#${name} .dropDown`);
         const options = Array.from(document.querySelector(`#${name} > .dropDown`).children);
 
-        selector.addEventListener("click", () => {
-            dropDown.hidden = false;
-        })
-
-        window.addEventListener("click", (e) => {
+        this.whenClicked(selector, () => {dropDown.hidden = false});
+        this.whenClicked(window, (e) => {
             if (options.includes(e.target)) console.log(e.target.textContent);
             if (e.target !== dropDown && e.target !== selector) dropDown.hidden = true;
-        })
+        });
+    }
+
+    whenClicked(source, action) {
+        source.addEventListener("click", action);
     }
 }
 
